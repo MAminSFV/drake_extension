5fe96a0e841766e8e0df155acb273f5acacaa6ab
M. Amin Safavi <safavi.m.amin@gmail.com>
2024-09-01 17:50:28 +0900

    feat: first working version - no doc

---
+++ tests/test_custom_system.py
@@ -0,0 +1,29 @@
+import pytest
+import numpy as np
+import custom_system
+
+from pydrake.systems.analysis import Simulator
+from pydrake.systems.framework import (
+    DiagramBuilder,
+)
+from pydrake.systems.primitives import (
+    ConstantVectorSource,
+    VectorLogSink,
+)
+
+
+def test_custom_system():
+    builder = DiagramBuilder()
+    source = builder.AddSystem(ConstantVectorSource([10.]))
+    adder = builder.AddSystem(custom_system.SimpleAdder(100.))
+    builder.Connect(source.get_output_port(0), adder.get_input_port(0))
+    logger = builder.AddSystem(VectorLogSink(1))
+    builder.Connect(adder.get_output_port(0), logger.get_input_port(0))
+    diagram = builder.Build()
+
+    simulator = Simulator(diagram)
+    simulator.AdvanceTo(1)
+
+    x = logger.FindLog(simulator.get_context()).data()
+    print("Output values: {}".format(x))
+    assert np.allclose(x, 110.)
