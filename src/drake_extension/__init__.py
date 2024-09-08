b0d31fa810365cecd608d97c893cfc0c3be0acbd
M. Amin Safavi <safavi.m.amin@gmail.com>
2024-09-08 22:08:07 +0900

    Update __init__.py

--- src/drake_extension/__init__.py
+++ src/drake_extension/__init__.py
@@ -1,11 +1,4 @@
 __all__ = ["SimpleAdder"]
 
-from pathlib import Path
-from typing import Any, Dict, Optional, Tuple, Union, List
-
-from numpy import float_
-from numpy.typing import NDArray
-from pydrake.systems.framework import LeafSystem
-
 from . import custom_system
 from .custom_system import SimpleAdder
