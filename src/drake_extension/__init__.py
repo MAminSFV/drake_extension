8a52114fb03725bdebe1095fbb7e116d85356b7b
M. Amin Safavi <safavi.m.amin@gmail.com>
2024-09-12 23:50:30 +0900

    Update __init__.py

--- src/drake_extension/__init__.py
+++ src/drake_extension/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from pydrake.systems.framework import LeafSystem
 from .drake_extension_ext import __doc__, SimpleAdder
 
-__all__ = ["__doc__", "SimpleAdder"]
+__all__ = ["__doc__", "SimpleAdder", "LeafSystem"]
